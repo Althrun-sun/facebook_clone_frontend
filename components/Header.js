62962049933d587c662d1027b756d1dcf0c59f19
althrun <althrunsun@gmail.com>
2023-02-09 12:33:35 -0800

    facebook login

--- components/Header.js
+++ components/Header.js
@@ -1,70 +1,97 @@
-import Image from 'next/image'
-import React from 'react'
-import {HiOutlineHome, HiOutlineSearch} from "react-icons/hi"
-import {RiFlag2Line} from "react-icons/ri"
-import {MdOutlineOndemandVideo,MdOutlineExpandMore} from "react-icons/md"
-import {AiOutlineShop,AiFillMessage,AiFillBell} from "react-icons/ai"
-import {IoGameControllerOutline} from "react-icons/io5"
-import {CgMenuGridO} from "react-icons/cg"
-
+import Image from "next/image";
+import React from "react";
+import { HiOutlineHome, HiOutlineSearch } from "react-icons/hi";
+import { RiFlag2Line } from "react-icons/ri";
+import { MdOutlineOndemandVideo, MdOutlineExpandMore } from "react-icons/md";
+import { AiOutlineShop, AiFillMessage, AiFillBell } from "react-icons/ai";
+import { IoGameControllerOutline } from "react-icons/io5";
+import { CgMenuGridO } from "react-icons/cg";
+import { signOut, useSession } from "next-auth/react";
 
 const Header = () => {
+  const { data: session } = useSession();
   return (
-    <div className='bg-white flex items-center p-2 shadow-md top-0 sticky z-50 h-16'>
+    <div className="bg-white flex items-center p-2 shadow-md top-0 sticky z-50 h-16">
       {/* Left */}
-      <div className='flex min-w-fit'>
+      <div className="flex min-w-fit">
         <Image
-        src="https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/1920px-Facebook_f_logo_%282021%29.svg.png"
-        height={40}
-        width={40}
+          src="https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/1920px-Facebook_f_logo_%282021%29.svg.png"
+          height={40}
+          width={40}
+          alt="fbIcon"
         />
-        <div className='flex items-cente space-x-2 px-2 ml-2 py-2 rounded-full bg-gray-100 text-gray-500'>
-          <HiOutlineSearch size={20}/>
-          <input className='hidden lg:inline-flex bg-transparent focus:outline-none' type="text" placeholder='Search Facebook'></input>
+        <div className="flex items-cente space-x-2 px-2 ml-2 py-2 rounded-full bg-gray-100 text-gray-500">
+          <HiOutlineSearch size={20} />
+          <input
+            className="hidden lg:inline-flex bg-transparent focus:outline-none"
+            type="text"
+            placeholder="Search Facebook"
+          ></input>
         </div>
       </div>
       {/* Centre */}
-      <div className='flex flex-grow justify-center mx-2'>
-        <div className='flex items-center'>
-          <div className='flex items-center h-14 px-4 md:px-10 round-md md:hover:bg-gray-100 cursor-pointer'>
-            <HiOutlineHome className='mx-auto' size={25}></HiOutlineHome>
+      <div className="flex flex-grow justify-center mx-2">
+        <div className="flex items-center">
+          <div className="flex items-center h-14 px-4 md:px-10 round-md md:hover:bg-gray-100 cursor-pointer">
+            <HiOutlineHome className="mx-auto" size={25}></HiOutlineHome>
           </div>
-          <div className='flex items-center h-14 px-4 md:px-10 round-md md:hover:bg-gray-100 cursor-pointer'>
-            <RiFlag2Line className='mx-auto' size={25}></RiFlag2Line>
+          <div className="flex items-center h-14 px-4 md:px-10 round-md md:hover:bg-gray-100 cursor-pointer">
+            <RiFlag2Line className="mx-auto" size={25}></RiFlag2Line>
           </div>
-          <div className='flex items-center h-14 px-4 md:px-10 round-md md:hover:bg-gray-100 cursor-pointer'>
-            <MdOutlineOndemandVideo className='mx-auto' size={25}></MdOutlineOndemandVideo>
+          <div className="flex items-center h-14 px-4 md:px-10 round-md md:hover:bg-gray-100 cursor-pointer">
+            <MdOutlineOndemandVideo
+              className="mx-auto"
+              size={25}
+            ></MdOutlineOndemandVideo>
           </div>
-          <div className='flex items-center h-14 px-4 md:px-10 round-md md:hover:bg-gray-100 cursor-pointer'>
-            <AiOutlineShop className='mx-auto' size={25}></AiOutlineShop>
+          <div className="flex items-center h-14 px-4 md:px-10 round-md md:hover:bg-gray-100 cursor-pointer">
+            <AiOutlineShop className="mx-auto" size={25}></AiOutlineShop>
           </div>
-          <div className='flex items-center h-14 px-4 md:px-10 round-md md:hover:bg-gray-100 cursor-pointer'>
-            <IoGameControllerOutline className='mx-auto' size={25}></IoGameControllerOutline>
+          <div className="flex items-center h-14 px-4 md:px-10 round-md md:hover:bg-gray-100 cursor-pointer">
+            <IoGameControllerOutline
+              className="mx-auto"
+              size={25}
+            ></IoGameControllerOutline>
           </div>
         </div>
-      </div> 
-      {/* Right */}
-      <div className='flex items-center justify-end min-w-fit space-x-2'>
-      <Image
-        src="https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/1920px-Facebook_f_logo_%282021%29.svg.png"
-        height={40}
-        width={40}
-        />
-        <p className='hidden xl:inline-flex font-semibold text-sm whitespace-nowrap p-3 max-w-xs'>
-          Althrun
-        </p>
-        <CgMenuGridO size={20} className="hidden lg:inline-flex h-10 w-10 bg-gray-200 text-gray-600
-        rounded-full p-2 cursor-pointer hover:bg-gray-300"></CgMenuGridO>
-        <AiFillMessage size={20} className="hidden lg:inline-flex h-10 w-10 bg-gray-200 text-gray-600
-        rounded-full p-2 cursor-pointer hover:bg-gray-300"></AiFillMessage>
-        <AiFillBell size={20} className="hidden lg:inline-flex h-10 w-10 bg-gray-200 text-gray-600
-        rounded-full p-2 cursor-pointer hover:bg-gray-300"></AiFillBell>
-        <MdOutlineExpandMore size={20} className="hidden lg:inline-flex h-10 w-10 bg-gray-200 text-gray-600
-        rounded-full p-2 cursor-pointer hover:bg-gray-300"></MdOutlineExpandMore>
       </div>
-
+      {/* Right */}
+      <div className="flex items-center justify-end min-w-fit space-x-2">
+        <Image
+          onClick={signOut}
+          className="rounded-full cursor-pointer"
+          alt="userImage"
+          priority={true}
+          src={session?.user.image}
+          height={40}
+          width={40}
+        />
+        <p className="hidden xl:inline-flex font-semibold text-sm whitespace-nowrap p-3 max-w-xs">
+          {session?.user.name.split(" ")[0]}
+        </p>
+        <CgMenuGridO
+          size={20}
+          className="hidden lg:inline-flex h-10 w-10 bg-gray-200 text-gray-600
+        rounded-full p-2 cursor-pointer hover:bg-gray-300"
+        ></CgMenuGridO>
+        <AiFillMessage
+          size={20}
+          className="hidden lg:inline-flex h-10 w-10 bg-gray-200 text-gray-600
+        rounded-full p-2 cursor-pointer hover:bg-gray-300"
+        ></AiFillMessage>
+        <AiFillBell
+          size={20}
+          className="hidden lg:inline-flex h-10 w-10 bg-gray-200 text-gray-600
+        rounded-full p-2 cursor-pointer hover:bg-gray-300"
+        ></AiFillBell>
+        <MdOutlineExpandMore
+          size={20}
+          className="hidden lg:inline-flex h-10 w-10 bg-gray-200 text-gray-600
+        rounded-full p-2 cursor-pointer hover:bg-gray-300"
+        ></MdOutlineExpandMore>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,20 +5,23 @@ import Sidebaritem from './Sidebaritem'
 import {MdGroups,MdOutlineOndemandVideo,MdOutlineExpandMore} from "react-icons/md"
 import {AiOutlineShop} from "react-icons/ai"
 import {BsStopwatch} from "react-icons/bs"
+import { useSession } from 'next-auth/react'
 
 
 const Sidebar = () => {
+  const {data:session}= useSession();
   return (
     <div className='hidden lg:inline-flex flex-col py-2 pl-2 max-w-xl lg:min-w-[302px]'>
         <div className='flex items-center space-x-2 py-3 pl-4
          hover:bg-gray-200 rounded-l-xl cursor-pointer'> 
         <Image
-        src="https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/1920px-Facebook_f_logo_%282021%29.svg.png"
+        alt='fbIcon'
+        src={session?.user.image}
         height={40}
         width={40}
-        className="rouned-full cursor-pointer"
+        className="rounded-full cursor-pointer"
         /> 
-        <p className='hidden sm:inline-flex font-medium'> Althrun Sun</p>
+        <p className='hidden sm:inline-flex font-medium'> {session?.user.name}</p>
         </div>
        <Sidebaritem Icon={ImUsers} value="Friends"  ></Sidebaritem>
        <Sidebaritem Icon={MdGroups} value="Groups"  ></Sidebaritem>
