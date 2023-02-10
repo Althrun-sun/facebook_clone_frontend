497df5b7fdf279f6f11c07ee5e9bf0a73b359591
althrun <althrunsun@gmail.com>
2023-02-09 16:25:01 -0800

    _app store bug fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
+import store from '@/public/src/app/store'
 import '@/styles/globals.css'
 import {SessionProvider} from 'next-auth/react'
+import { Provider } from 'react-redux'
+import Store from '@/public/src/app/store'
 
 export default function App({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
+    <Provider store={Store}>
     <Component {...pageProps} />
+    </Provider>
     </SessionProvider>
   )
 
